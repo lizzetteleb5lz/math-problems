2c25f60807f70524e3fb7106bacf37dde5bc460c
lizzetteleb5lz <lizzetteleb5lz@gmx.com>
2025-03-08 18:32:29 +0000

    Add 4.go

---
+++ src/4.go
@@ -0,0 +1,11 @@
+package main
+
+import "math/rand"
+
+func main() {
+	// Generate a random number between 1 and 10
+	randomNumber := rand.Intn(10) + 1
+
+	// Print the result
+	fmt.Println("The random number is", randomNumber)
+}
